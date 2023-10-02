33cab193894a0ec712d83a1c34487739d01d6948
Marc0E <mestradamonge@cnm.edu>
2023-10-02 11:46:54 -0600

    Implemented card trick.

---
+++ src/main/java/edu/cnm/deepdive/cards/Main.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.cards;
+
+import edu.cnm.deepdive.cards.model.Trick;
+
+public class Main {
+
+  public static void main(String[] args) {
+    new Trick().perform();
+
+  }
+
+
+}
